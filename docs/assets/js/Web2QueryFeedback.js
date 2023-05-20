e1fb982cd3920a786fe4ea334bafb55741f8b3a2
cdemmigs <cdemmings@gmail.com>
2023-05-19 23:59:27 -0400

    Select2Query feedback form history

--- docs/assets/js/Web2QueryFeedback.js
+++ docs/assets/js/Web2QueryFeedback.js
@@ -1,15 +1,57 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     const form = document.getElementById('select2queryfeedback-form');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      .then(() => {
-        alert("Success!");
-      })
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const data = new FormData(form);
+        const action = e.target.action;
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        })
+            .then(() => {
+                alert("Success!");
+            })
     });
-  });
+
+
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    fetchComments();
+});
+
+
+function fetchComments() {
+    const appsScriptGetFeedbackUrl = "https://script.google.com/macros/s/AKfycbxuZ5cu_gWo0g-q1B3JYTlJ41cbSdJAWlj3Y7lNHj83aFo8OkVM28XYpotk8UBbVr9x/exec";
+    const projectFilter = "?project=Select2Query";
+    const fetchUrl = appsScriptGetFeedbackUrl + projectFilter;
+    fetch(fetchUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (commentData) {
+        const userComments = commentData.GoogleSheetData;
+
+        const commentsHeaderPlaceHolder = document.querySelector("#data-header");
+        let commentHeaderOut = "";
+        let headerRow =  userComments[0];
+        for (let colName of headerRow) {
+            commentHeaderOut += `<th>${colName}</th>`    
+        }
+        commentsHeaderPlaceHolder.innerHTML = commentHeaderOut;
+
+        const commentsPlaceHolder = document.querySelector("#data-output");
+        let out = "";          
+        for (let i = 1; i < userComments.length; i++) {
+            const singleComment = userComments[i];
+
+            out += '<tr style="background-color:#fafbfc">';
+            for (let colData of singleComment) {
+                out += `<td>${colData}</td>`;
+            }
+            out += "</tr>";
+        }
+
+        commentsPlaceHolder.innerHTML = out;
+    });
+}
--- docs/notes/select2queryfeedback.markdown
+++ docs/notes/select2queryfeedback.markdown
@@ -5,14 +5,15 @@ date:   2023-05-13 12:00:00 -0500
 categories: QUERY
 ---
 
+
+
 ## Post Feedback
 
 
 <form 
   id="select2queryfeedback-form"
   method="POST" 
-  action="https://script.google.com/macros/s/AKfycbwsoqs0cFeBCpiMsX5Eo8syGQJNIEOj-fq7hy7N5-XzCpLMNrDYUlYAku7OMKojNxfi/exec"
-  
+  action="https://script.google.com/macros/s/AKfycbwsoqs0cFeBCpiMsX5Eo8syGQJNIEOj-fq7hy7N5-XzCpLMNrDYUlYAku7OMKojNxfi/exec"  
 >
   <label for="eMail"><b>eMail address </b>(optional)</label><br>
   <input name="eMail" type="email" placeholder="eMail" >
@@ -32,3 +33,25 @@ categories: QUERY
 
    <script src="{{ '/assets/js/Web2QueryFeedback.js' | relative_url }}"></script>
 </form>
+
+<br>
+
+
+## Previous Feedback
+
+
+<table>
+
+      <thead>
+         <tr id="data-header">
+            <!-- Table header columns from javascript file in here. -->
+         </tr>
+      </thead>
+      <tbody id="data-output">
+         <!-- Comments from javascript file in here. -->
+      </tbody>
+ 
+</table>
+
+
+
